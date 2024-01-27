272f90cf931cf78fbcf737b69d0dd84e1f5841e4
HaziimYassien <haziim.yassien@gmail.com>
2024-01-27 16:26:34 +0200

    finalizing

--- LosPollosHermanos/LosPollosHermanos/Scripts/script.js
+++ LosPollosHermanos/LosPollosHermanos/Scripts/script.js
@@ -1,1 +1,30 @@
-﻿
+﻿document.getElementById('exampleCheck1').addEventListener('change', function () {
+    var signInButtonContainer = document.getElementById('signInButtonContainer');
+    var signInButton = document.getElementById('signInButton');
+
+    if (this.checked) {
+        signInButtonContainer.style.display = 'block';
+    } else {
+        signInButtonContainer.style.display = 'none';
+    }
+});
+
+var former = document.getElementsByClassName("former")
+former.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+document.getElementById('exampleInputEmail1').addEventListener('input', function () {
+    var inputValue = this.value.toLowerCase();
+
+    // Check if the input contains the word "mike"
+    if (inputValue.includes('half measures')) {
+        document.body.style.backgroundImage = 'url("../Images/Mike_Los_Pollos_Truck.png")';
+        former.style.backgroundImage = 'url("../Images/Mike_Los_Pollos_Truck.png")';
+    }
+
+    else if (inputValue.includes('a cautious man')) {
+        document.body.style.backgroundImage = 'url("../Images/a catious man 3.gif")';
+    }
+    else {
+        // Reset to the original background image
+        document.body.style.backgroundImage = 'url("../Images/242731706_629385025114450_882571064577353266_n.jpg")';
+    }
+});
